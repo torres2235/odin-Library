7c3032e495c53cb5c571567840404540ce048663
Torres2235 <Torres2235@gmail.com>
2021-11-01 13:38:38 -0700

    working on NEW BOOK button

--- js-file.js
+++ js-file.js
@@ -1,13 +1,41 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
+    /*
     this.info = function() {
         return(title + " by " + author + ", " + pages + " pages, " + read);
     }
+    */
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "not read yet");
-console.log(theHobbit.info());
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
+const sevenHabits = new Book("The 7 Habits of Highly Effective People", "Stephen Covey", 381, false);
+const nineteenEightFour = new Book("1984", "George Orwell", 328, true);
+addBookToLibrary(theHobbit);
+addBookToLibrary(sevenHabits);
+addBookToLibrary(nineteenEightFour);
+
+console.table(myLibrary);
+
+const newBook = document.querySelector('#new');
+newBook.addEventListener('click', () => {
+    let title = prompt('Book Title: ', 'insert title');
+    let author = prompt('Book Author: ', 'insert author');
+    let pages = prompt('Book Pages: ', 999);
+    pages = parseInt(pages);
+    let read = confirm('Have you read it?');
+
+    const newBook = new Book (title, author, pages, read);
+    addBookToLibrary(newBook);
+    console.table(myLibrary);
+});
